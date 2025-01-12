49761ccf02de3c81abc94512b801c13c90207822
Ilyass Chahbia <ilchahbi@e1r8p7.1337.ma>
2025-01-12 16:35:54 +0100

    1 to 1

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: ilchahbi <ilchahbi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 10:04:49 by ilchahbi          #+#    #+#             */
-/*   Updated: 2025/01/11 10:12:30 by ilchahbi         ###   ########.fr       */
+/*   Updated: 2025/01/12 16:35:11 by ilchahbi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,26 @@
 
 int main(int ac, char **av)
 {
-    int i;
+    int			n;
+    int         m;
+    std::string res;
+    
+    n = 1;
+    if (ac < 2)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else
+    {
+        while (av[n])
+		{
+			res = av[n];
+            m = 0;
+            while (res[m])
+            {
+			    std::cout << (char)std::toupper(res[m]);
+                m++;
+            }
+			n++;
+		}
+        std::cout << std::endl;
+    }
 }
