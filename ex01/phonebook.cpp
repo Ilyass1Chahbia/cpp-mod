af1a30bc883ddbf4f16ff45f63407f2cffaa0891
Ilyass1Chahbia <ilyass@LENOVO.>
2025-01-19 11:23:20 +0100

    fixed empty str

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -17,42 +17,74 @@ int main()
 	phonebook	bk;
 	int         nb;
 	std::string input;
+	int			v;
+
 	while (1)
 	{
 		contact   	pb;
+		v = 0;
 		if (std::cin.eof())
 			exit(1);
 		std::cout << "Select ADD, SEARCH or EXIT : " << std::endl;
 		std::getline(std::cin, input);
 		if (input == "ADD")
 		{
-			if (std::cin.eof())
-				exit(1);
-			std::cout << "Enter First Name : ";
-			std::getline(std::cin, input);
-			pb.getfname(input);
-			if (std::cin.eof())
-				exit(1);
-			std::cout << "Enter Last Name : ";
-			std::getline(std::cin, input);
-			pb.getlname(input);
-			if (std::cin.eof())
-				exit(1);
-			std::cout << "Enter Nick-Name : ";
-			std::getline(std::cin, input);
-			pb.getnname(input);
-			if (std::cin.eof())
-				exit(1);
-			std::cout << "Enter Phone-Number : ";
-			std::getline(std::cin, input);
-			if (pb.allisdig(input))
-				continue;
-			pb.getnum(input);
-			if (std::cin.eof())
-				exit(1);
-			std::cout << "Enter Darkest Secret : ";
-			std::getline(std::cin, input);
-			pb.getdsec(input);
+			while (!v)
+			{
+				if (std::cin.eof())
+					exit(1);
+				std::cout << "Enter First Name : ";
+				std::getline(std::cin, input);
+				pb.getfname(input);
+				if (!input.empty() && input.find_first_not_of(" \t") != std::string::npos)
+                    v = 1;
+			}
+			v = 0;
+			while (!v)
+			{
+				if (std::cin.eof())
+					exit(1);
+				std::cout << "Enter Last Name : ";
+				std::getline(std::cin, input);
+				pb.getlname(input);
+				if (!input.empty() && input.find_first_not_of(" \t") != std::string::npos)
+                    v = 1;
+			}
+			v = 0;
+			while (!v)
+			{
+				if (std::cin.eof())
+					exit(1);
+				std::cout << "Enter Nick-Name : ";
+				std::getline(std::cin, input);
+				pb.getnname(input);
+				if (!input.empty() && input.find_first_not_of(" \t") != std::string::npos)
+                    v = 1;
+			}
+			v = 0;
+			while (!v)
+			{
+				if (std::cin.eof())
+					exit(1);
+				std::cout << "Enter Phone-Number : ";
+				std::getline(std::cin, input);
+				if (pb.allisdig(input))
+					continue;
+				pb.getnum(input);
+				if (!input.empty() && input.find_first_not_of(" \t") != std::string::npos)
+                    v = 1;
+			}
+			v = 0;
+			while (!v)
+			{
+				if (std::cin.eof())
+					exit(1);
+				std::cout << "Enter Darkest Secret : ";
+				std::getline(std::cin, input);
+				pb.getdsec(input);
+				if (!input.empty() && input.find_first_not_of(" \t") != std::string::npos)
+                    v = 1;
+			}
 			bk.writebook(pb);
 		}
 		else if (input == "SEARCH")
